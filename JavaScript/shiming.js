411b3cdab4d5b0ae9bbd7fefecf87ab19a3fa368
xza <584788096@qq.com>
2018-10-16 16:51:06 +0800

    实名认证完善

--- JavaScript/shiming.js
+++ JavaScript/shiming.js
@@ -1,7 +1,8 @@
 (function(){
-    replaceImg("#input-idcard-obverse",'#idcard-obverse',"change-idcard-obverse-btn")
-    replaceImg("#input-idcard-reverse",'#idcard-reverse',"change-idcard-reverse-btn")
-    function replaceImg(a,b,c){
+    replaceImg("#input-idcard-obverse",'#idcard-obverse')
+    replaceImg("#input-idcard-reverse",'#idcard-reverse')
+    let imageList={}
+    function replaceImg(a,b){
         $(a).on("change", function (e) {
             var fileInput = $(a)[0];        
             var file = fileInput.files[0];        //创建读取文件的对象
@@ -10,7 +11,8 @@
             if(file.type.indexOf("image") < 0){
                 alert('请上传图片文件')
                 return
-            }        
+            }
+            imageList[a.slice(a.indexOf('-')+1)]  = file;      
             var reader = new FileReader();                 //创建文件读取相关的变量       
             var imgFile;                 //为文件读取成功设置事件        
             reader.onload=function(e) {            
@@ -19,35 +21,51 @@
                 $(b).attr('src',imgFile)
             };                 
             reader.readAsDataURL(file);
-            document.getElementById(c).onclick=function(){
-                let tempForm = new FormData();
-                console.log(file)
-                tempForm.append("xxx","file");
-                tempForm.append("fileImg",file);
-                //append添加要传输的内容
-                $.ajax({
-                    type: "post",
-                    url: "/xxx",
-                    data: tempForm,
-                    contentType: 'multipart/form-data', 
-                    processData: false,    //false
-                    cache: false,    //缓存
-                    beforeSend:function(){
-                        $('.loading').addClass("active")
-                    },
-                    success: function(data){
-                        console.log(data);      
-                    },
-                    fail:function(){
-                        console.log('error')
-                    },
-                    complete:function(){
-                        setTimeout(function(){
-                            $('.loading').removeClass("active")
-                        },1000)
-                    }
-              })
-            }
         })
     }
+    $("#true-id-sumbit").on("click",function(){
+        let imageFile=Object.getOwnPropertyNames(imageList);
+        console.log(imageFile)
+        if(imageFile.length !== 2){
+            alert("请上传图片文件")
+            return
+        }
+        let tempForm = new FormData();
+        imageFile.forEach(function(e){//上传图片内容
+            tempForm.append(e,imageList[e]);
+        })
+        function temp(a){ //获取上传内容
+            if(document.getElementById(a).value === ""){
+                alert("请完善内容")
+                return 
+            }
+            return document.getElementById(a).value
+        }
+        tempForm.append("itype",temp("itype"));
+        tempForm.append("true-name",temp("true-name"));
+        tempForm.append("id-number",temp("id-number"));
+        //append添加要传输的内容
+        $.ajax({
+            type: "post",
+            url: "/xxx",
+            data: tempForm,
+            contentType: 'multipart/form-data', 
+            processData: false,    //false
+            cache: false,    //缓存
+            beforeSend:function(){
+                $('.loading').addClass("active")
+            },
+            success: function(data){
+                console.log(data);      
+            },
+            fail:function(){
+                console.log('error')
+            },
+            complete:function(){
+                setTimeout(function(){
+                    $('.loading').removeClass("active")
+                },1000)
+            }
+      })
+    })
 }())
