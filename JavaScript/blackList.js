baa4c15b0427732eda21e95ff9a9057c22cadc01
xza <584788096@qq.com>
2018-10-18 19:53:06 +0800

    黑名单分页循环加载

--- JavaScript/blackList.js
+++ JavaScript/blackList.js
@@ -26,32 +26,48 @@
 //加载进页面时和点击1号页面行为相同
 //之后点击页面都返回
 //模拟接收的时json数据
-var bList ='{"code":200,"message":null,"data":{"total":8,"size":5,"pages":2,"current":1,"records":[{"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"}]}}'
-console.log(JSON.parse(testboke))
+var bList ='{"code":200,"message":null,"data":{"total":8,"size":5,"pages":2,"current":1,"records":[{"uid":1,"head":"Images/head.jpg","userName":"userName","time":"2018-06-30 02:44:21"},{"uid":2,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":3,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":4,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":5,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":6,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":7,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":8,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"}]}}'
+//----下面时成功收到时执行的内容
 bList = JSON.parse(bList)
+let temp={}
+temp.size = 5
+temp.data = bList.data.records
+console.log(bList.data.records)
+temp.domValue = ``
+temp.data.forEach(function(e){
+    temp.domValue += `<main>
+                    <div class="blackList-item">
+                        <div class="blackList-item-lt">
+                            <div>
+                                <img src="${e.head}">
+                            </div>
+                            <div class="blackList-item-lt-message text-left">
+                                <div>
+                                    <p>${e.userName}</p>
+                                </div>
+                                <div>
+                                    <p>添加时间：${e.time}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blackList-item-rt">
+                            <button value="${e.uid}" class="btn btn-default">移除</button>
+                        </div>
+                    </div>
+                    </main>`
+})
 
-bList.size = bList.data.total/5
-function (){
+temp.domNav = ''
+temp.current = bList.data.total
+console.log(Math.ceil(bList.data.total/temp.size))
+if(bList.data.total <= temp.size*5){
+    for(let i=1;i<=Math.ceil(bList.data.total/temp.size);i++)
+    temp.domNav += `<li><a href="#">${i}</a></li>`
+}
+console.log(temp.domNav)
+console.log(temp.domValue)
+function mkTemplate(){
     return `
-    <main class="blackList-content">
-        <div class="blackList-item">
-            <div class="blackList-item-lt">
-                <div>
-                    <img src="Images/head.jpg">
-                </div>
-                <div class="blackList-item-lt-message text-left">
-                    <div>
-                        <p>user-name</p>
-                    </div>
-                    <div>
-                        <p>添加时间：2018-06-30 02:44:21</p>
-                    </div>
-                </div>
-            </div>
-            <div class="blackList-item-rt">
-                <button class="btn btn-default">移除</button>
-            </div>
-        </div>
         <nav aria-label="Page navigation">
             <ul class="pagination">
                 <li>
@@ -70,7 +86,6 @@ function (){
                 </li>
             </ul>
         </nav>
-    </main>
     `
 }
 }())
