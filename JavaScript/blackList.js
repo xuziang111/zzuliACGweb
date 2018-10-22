60c977f78b3117fbdf17b5b763d71860316ddaef
xza <584788096@qq.com>
2018-10-22 16:37:06 +0800

    黑名单页面完成

--- JavaScript/blackList.js
+++ JavaScript/blackList.js
@@ -1,17 +1,21 @@
 (function(){
-    $("blackList-item-rt>button").on("click",function(e){
-        console.log(e)
+
+    $("#bl-nav").on("click",function(e){
+        if(e.target.tagName === 'SPAN'){
+            let temp ={}
+            temp['needPage'] = $(e.target).attr('data-page')
+            temp = JSON.stringify(temp)
         $.ajax({
             type: "post",
-            url: "/removeBlacklist",
-            data: tempForm, 
+            url: "/loadingBlacklist",
+            data: temp, 
             processData: false,    //false
             cache: false,    //缓存
             beforeSend:function(){
                 $('.loading').addClass("active")
             },
             success: function(data){
-                console.log(data);      
+                ajaxSuccess(data);      
             },
             fail:function(){
                 console.log('error')
@@ -22,20 +26,51 @@
                 },1000)
             }
         })
+    }
     })
-//加载进页面时和点击1号页面行为相同
-//之后点击页面都返回
+
+    $("#blackList-content").on("click",function(e){
+        if(e.target.tagName === 'BUTTON'){
+        let temp = {removeBl:$(e.target).attr('value')}
+        temp= JSON.stringify(temp)
+        $.ajax({
+            type: "post",
+            url: "/removeBlacklist",
+            data: pageData, 
+            processData: false,    //false
+            cache: false,    //缓存
+            beforeSend:function(){
+                $('.loading').addClass("active")
+            },
+            success: function(data){//重新接收数据
+                console.log('成功移除')
+                ajaxSuccess(data)      
+            },
+            fail:function(){
+                console.log('error')
+            },
+            complete:function(){
+                setTimeout(function(){
+                    $('.loading').removeClass("active")
+                },1000)
+            }
+        })
+    }
+    })
+
+
 //模拟接收的时json数据
-var bList ='{"code":200,"message":null,"data":{"total":8,"size":5,"pages":2,"current":1,"records":[{"uid":1,"head":"Images/head.jpg","userName":"userName","time":"2018-06-30 02:44:21"},{"uid":2,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":3,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":4,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":5,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":6,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":7,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"},{"uid":8,"head":"Images/head.jpg","userNname":"userName","time":"2018-06-30 02:44:21"}]}}'
+var bList ='{"code":200,"message":null,"data":{"total":8,"size":5,"pages":2,"current":1,"records":[{"uid":1,"head":"Images/head.jpg","userName":"userName","time":"2018-06-30 02:44:21"},{"uid":2,"head":"Images/head.jpg","userName":"userName","time":"2018-06-30 02:44:21"},{"uid":3,"head":"Images/head.jpg","userName":"userName","time":"2018-06-30 02:44:21"},{"uid":4,"head":"Images/head.jpg","userName":"userName","time":"2018-06-30 02:44:21"},{"uid":5,"head":"Images/head.jpg","userName":"userName","time":"2018-06-30 02:44:21"}]}}'
 //----下面时成功收到时执行的内容
-bList = JSON.parse(bList)
-let temp={}
-temp.size = 5
-temp.data = bList.data.records
-console.log(bList.data.records)
-temp.domValue = ``
-temp.data.forEach(function(e){
-    temp.domValue += `<main>
+function ajaxSuccess(bList){
+    bList = JSON.parse(bList)
+    let temp={}
+    temp.size = 5
+    console.log(bList.data.records)
+    temp.domValue = ``
+    temp.data = bList.data.records
+    temp.data.forEach(function(e){
+    temp.domValue += `
                     <div class="blackList-item">
                         <div class="blackList-item-lt">
                             <div>
@@ -54,91 +89,58 @@ temp.data.forEach(function(e){
                             <button value="${e.uid}" class="btn btn-default">移除</button>
                         </div>
                     </div>
-                    </main>`
-})
+                    `
+    })
+    temp.domNav = ``
+    temp.current = bList.data.current
+    temp.total = bList.data.total
+    temp.page = Math.ceil(bList.data.total/temp.size)
 
-temp.domNav = ''
-temp.current = bList.current
-temp.total = bList.data.total
-temp.page = Math.ceil(bList.data.total/temp.size)
-function makeLi(i,current){//生成标签页
-    let tp = ''
-    if(i===current){//给当前页面标签加上.active}
-        tp += `<li class="active"><a data-page="${i}" href="#">${i}</a></li>`
-            }else{
-        tp += `<li><a data-page="${i}" href="#">${i}</a></li>`
+    temp.domNav = makeLi(temp,bList)
+    //将生成元素插入到页面中
+    $('.blackList-content').prepend(temp.domValue)
+    $('#bl-nav>.pagination').append(temp.domNav)
+}
+
+function makeLiFor(a,b,current){//循环生成li
+    let domNav=``
+    for(let i=a;i<=b;i++){
+        if(i===current){//给当前页面标签加上.active}
+                domNav = domNav +  `<li class="active"><span data-page="${i}" href="#">${i}</span></li>`
+        }else{
+                domNav = domNav + `<li class=""><span data-page="${i}" href="#">${i}</span></li>`
+        }
     }
-    return tp
+    return domNav
 }
 //-------
-{
-let a,b
-if(bList.data.total <= temp.size*5){//生成要渲染的分页标签
-    //标签页小于等于5时
-    for(let i=1;i<=temp.page ;i++){
-        temp.domNav = temp.domNav + makeLi(i,temp.current)
-    }
-}else if(bList.data.total > temp.size*5){
-    //总标签页大于5时
-    if(temp.current < 3){//当前标签页小于5时
-        for(let i=1;i<=temp.page ;i++){
-            temp.domNav = temp.domNav + makeLi(i,temp.current)
-        }
-    }else if(temp.current >= 3 && temp.current >= temp.page-2){//当前标签页大于最大标签页-2时
-        for(let i=temp.current-2;i<=temp.current+2 ;i++){
-            temp.domNav = temp.domNav + makeLi(i,temp.current)
-        }
-    }else if(temp.current > temp.page-2){
-        for(let i=temp.page-4;i<=temp.page ;i++){
-            temp.domNav = temp.domNav + makeLi(i,temp.current)
-        }
-    }
-}else if(bList.data.total > temp.size*5){
-    //总标签页大于5时
-    if(temp.current < 3){//当前标签页小于5时
+function makeLi(temp,bList){//生成要渲染的分页标签
+    console.log(temp.current)
+    let a,b
+    if(bList.data.total <= temp.size*5){//生成要渲染的分页标签
+        //标签页小于等于5时
         a=1;
-        b=temp.page;
-        for(let i=a;i<=b ;i++){
-            temp.domNav = temp.domNav + makeLi(i,temp.current)
-        }
-    }else if(temp.current >= 3 && temp.current >= temp.page-2){//当前标签页大于最大标签页-2时
-        a=temp.current-2;
-        b=temp.current+2;
-        for(let i=a;i<=b ;i++){
-            temp.domNav = temp.domNav + makeLi(i,temp.current)
-        }
-    }else if(temp.current > temp.page-2){
-        a=temp.page-4;
-        b=temp.page;
-        for(let i=a;i<=b ;i++){
-            temp.domNav = temp.domNav + makeLi(i,temp.current)
+        b=temp.page
+        return makeLiFor(a,b,temp.current)
+    }else if(bList.data.total > temp.size*5){
+        //总标签页大于5时
+        if(temp.current < 3){//当前标签页小于5时
+            a=1;
+            b=temp.page;
+            return  makeLiFor(a,b,temp.current)
+        }else if(temp.current >= 3 && temp.current >= temp.page-2){//当前标签页大于最大标签页-2时
+            a=temp.current-2;
+            b=temp.current+2;
+            return  makeLiFor(a,b,temp.current)
+        }else if(temp.current > temp.page-2){
+            a=temp.page-4;
+            b=temp.page;
+            return  makeLiFor(a,b,temp.current)
         }
     }
 }
-}
+
 //---------
-console.log(temp.domNav)
-console.log(temp.domValue)
-function mkTemplate(){
-    return `
-        <nav aria-label="Page navigation">
-            <ul class="pagination">
-                <li>
-                    <a href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                </li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-                <li><a href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    `
-}
+ajaxSuccess(bList)
+
 }())
