9ade7d6c1fb25e4901147fba7505b9bfe7512f45
xza <584788096@qq.com>
2018-10-31 16:04:33 +0800

    我的信息页面完成

--- src2/JavaScript/vue/wodexinxi.js
+++ src2/JavaScript/vue/wodexinxi.js
@@ -10,41 +10,41 @@ let wodexinxi = Vue.component('per-inf',{
         <div>
             <div class="form-group form-group-sm">
                 <label class="col-sm-2 control-label" for="username">昵称：</label>
-                <input class="form-control" v-model="username" type="text" id="username" placeholder="昵称">
+                <input class="form-control" v-model="userdata.username" type="text" id="username" placeholder="昵称">
             </div>
             <!-- 个性签名做处理防止xml注入 -->
             <div class="form-group form-group-sm">
                 <label class="col-sm-2 control-label" for="usermotto">个性签名：</label>
-                <input class="form-control" v-model="usermotto" type="text" id="usermotto" placeholder="个性签名">
+                <input class="form-control" v-model="userdata.usermotto" type="text" id="usermotto" placeholder="个性签名">
             </div>
             <div class="form-group form-group-sm">
-                <label class="col-sm-2 control-label" for="Birthday">出生日期：</label>
-                <input class="form-control" v-model="Birthday" type="date" id="Birthday" placeholder="如：1998-05-25">
+                <label class="col-sm-2 control-label" for="birthday">出生日期：</label>
+                <input class="form-control" v-model="userdata.birthday" type="date" id="birthday" placeholder="如：1998-05-25">
             </div>
             <div class="form-group form-group-sm sex-radio-container">
                 用户性别? XiuJi?：
-                <label class="radio-inline" :class="sexselect[0]">
-                     <input @click="sexclick" type="radio" v-model="sex" name="sex" id="inlineRadio1" value="0" hidden> 
+                <label class="radio-inline" :class="userdata.sexselect[0]">
+                     <input @click="sexclick" type="radio" v-model="userdata.sex" name="sex" id="inlineRadio1" value="0" hidden> 
                      <span class="sex-btn">猛汉</span>
                 </label>
-                <label class="radio-inline" :class="sexselect[1]">
-                    <input @click="sexclick" type="radio" v-model="sex" name="sex" id="inlineRadio2" value="1"> 
+                <label class="radio-inline" :class="userdata.sexselect[1]">
+                    <input @click="sexclick" type="radio" v-model="userdata.sex" name="sex" id="inlineRadio2" value="1"> 
                     <span class="sex-btn">萌妹</span>
                 </label>
-                <label class="radio-inline" :class="sexselect[2]">
-                    <input @click="sexclick" type="radio" v-model="sex" name="sex" id="inlineRadio2" value="2"> 
+                <label class="radio-inline" :class="userdata.sexselect[2]">
+                    <input @click="sexclick" type="radio" v-model="userdata.sex" name="sex" id="inlineRadio2" value="2"> 
                     <span class="sex-btn">保密</span>
                 </label>
             </div>
             <div class="form-group form-group-sm">
                 <label class="col-sm-2 control-label">上次登录IP：</label>
-                    <input class="form-control" v-model="ip" type="text" id="FromIP" placeholder="127.0.0.1" readonly>
+                    <input class="form-control" v-model="userdata.ip" type="text" id="FromIP" placeholder="127.0.0.1" readonly>
             </div>
             <div class="form-group form-group-sm">
                 <label class="col-sm-2 control-label" for="identity">用户身份：</label>
-                    <input class="form-control" v-model="identity" type="text" id="identity" placeholder="校外人士" readonly>
+                    <input class="form-control" v-model="userdata.identity" type="text" id="identity" placeholder="校外人士" readonly>
             </div>
-            <button id="false-id-sumbit" class="btn btn-default">保存</button>
+            <button @click="upload" id="false-id-sumbit" class="btn btn-default">保存</button>
             <!-- <div class="form-group form-group-sm active">
                 <label class="col-sm-2 control-label">院系 Faculty</label>
                 <div class="col-sm-4" readonly>
@@ -63,13 +63,15 @@ let wodexinxi = Vue.component('per-inf',{
     `,
     data:function(){
         return{
-            username:'啦啦啦',
-            usermotto:'个性签名',
-            Birthday:'1970-01-01',
-            sex:2,
-            sexselect:['','','active'],
-            ip:'127.0.0.1',
-            identity:'校外人士'
+            userdata:{
+                username:'啦啦啦',
+                usermotto:'个性签名',
+                birthday:'1970-01-01',
+                sex:2,
+                sexselect:['','',''],
+                ip:'127.0.0.1',
+                identity:'校外人士'
+            },
         }
     },
     methods:{
@@ -77,46 +79,33 @@ let wodexinxi = Vue.component('per-inf',{
             if(e.target.tagName === "INPUT"){
                 console.log(e)
                 console.log(e.target.value)
-                for(let i=0;i<this.sexselect.length;i++){
+                for(let i=0;i<this.userdata.sexselect.length;i++){
                     if(i === parseInt(e.target.value)){
                         console.log('xxx')
-                        this.sexselect[i] = 'active'
-                        this.sex=i
+                        this.userdata.sexselect[i] = 'active'
+                        this.userdata.sex=i
                     }else{
-                        this.sexselect[i] = ''
+                        this.userdata.sexselect[i] = ''
                     }
                 }
             }
-        }
-    },
-    mounted:function(){
-        $("#false-id-sumbit").on("click",function(){
-            // let tempForm = new FormData();
-            let data ={}
-            $("input[name='sex']").for
-            function temp(a){ //获取上传内容
-                if(document.getElementById(a).value === ""){
-                    return false
+        },
+        upload:function(){
+            console.log('')
+            if(this.userdata.username){
+                if(!(/^[\u4e00-\u9fa5_a-zA-Z0-9]{2,8}$/ig.test(this.userdata.username))){
+                    alert('昵称只能输入2-8位英文、数字、汉字或_组合')
+                    return
                 }
-                return document.getElementById(a).value
+            }else{
+                alert('请填写昵称')
+                return
             }
-                if(!(temp("username")&&temp("usermotto")&&temp("Birthday"))){
-                    alert("有内容未填写")
-                    return 
-                }
-            data.username=temp("username")
-            data.usermotto=temp("usermotto")
-            data.Birthday=temp("Birthday")
-            data.sex=$("input[name='sex']:checked").val()
-            data=JSON.stringify(data)//数据转json
-            // tempForm.append("username",temp("username"));
-            // tempForm.append("usermotto",temp("usermotto"));
-            // tempForm.append("Birthday",temp("Birthday"));
-            // tempForm.append("sex",$("input[name='sex']:checked").val())
-            //append添加要传输的内容
+            let user = document.cookie
+            let data={user:user,username:this.userdata.username,usermotto:this.userdata.usermotto,birthday:this.userdata.birthday,sex:this.userdata.sex}
             $.ajax({
                 type: "post",
-                url: "/xxx",
+                url: "/uploadpreson",
                 data: JSON.stringify(data),
                 processData: false,    //false
                 cache: false,    //缓存
@@ -135,6 +124,34 @@ let wodexinxi = Vue.component('per-inf',{
                     },1000)
                 }
           })
-        })
+
+        }
+    },
+    created:function(){
+        let user = document.cookie
+        _self = this
+        $.ajax({
+            type: "post",
+            url: "/downloadpreson",
+            data:user,
+            processData: false,    //false
+            cache: false,    //缓存
+            beforeSend:function(){
+                _self.userdata = Object.assign({}, _self.userdata, JSON.parse('{"username":"哈哈哈","usermotto":"个性签名2","birthday":"1971-01-01","sex":"2"}'))
+                _self.userdata.sexselect[_self.userdata.sex] = 'active'
+
+            },
+            success: function(data){
+                console.log(data);      
+            },
+            fail:function(){
+                console.log('error')
+            },
+            complete:function(){
+                setTimeout(function(){
+                    $('.loading').removeClass("active")
+                },1000)
+            }
+      })
     }
 })
