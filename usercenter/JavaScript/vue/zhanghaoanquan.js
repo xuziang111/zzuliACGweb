c5dfbba5be50016aed2c3c02bb8529124497dc5b
xza <584788096@qq.com>
2018-11-06 11:27:40 +0800

    账号安全列表循环

--- usercenter/JavaScript/vue/zhanghaoanquan.js
+++ usercenter/JavaScript/vue/zhanghaoanquan.js
@@ -9,40 +9,12 @@ let zhanghaoanquan = Vue.component('account-safe',{
     <div class="line-border"></div>
     <main>
         <ul>
-            <li class="safe-list-li">
+            <li class="safe-list-li" v-for="(userdatax,index) in this.userdatatemp">
                 <div class="container">
-                    <div class="col-xs-4 text-left"><i class="fa fa-fw fa-check-circle"></i>绑定邮箱</div>
-                    <div class="col-xs-4">{{this.userdata.emailvalue}}</div> 
-                    <div class="col-xs-4">更换邮箱</div> 
+                    <div class="col-xs-4 text-left"><i :class="userdatax.classname"></i>{{userdatax.lt}}</div>
+                    <div class="col-xs-4">{{userdatax.value}}</div> 
+                    <div class="col-xs-4">{{userdatax.rt}}</div> 
                 </div>
-            </li>
-            <li class="safe-list-li">
-                <div class="container">
-                    <div class="col-xs-4 text-left"><i class="fa fa-fw fa-check-circle"></i>绑定手机</div>
-                    <div class="col-xs-4">{{userdata.phonevalue}}</div> 
-                    <div class="col-xs-4">更换手机</div> 
-                </div>
-            </li>
-            <li class="safe-list-li">
-                <div class="container">
-                    <div class="col-xs-4 text-left"><i class="fa fa-fw fa-check-circle"></i>设置密码</div>
-                    <div class="col-xs-4">{{userdata.passwordvalue}}</div> 
-                    <div class="col-xs-4">更换密码</div> 
-                </div>
-            </li>
-            <li class="safe-list-li">
-                <div class="container">
-                    <div class="col-xs-4 text-left"><i class="fa fa-fw fa-exclamation-circle"></i>密保问题</div>
-                    <div class="col-xs-4">{{userdata.questionvalue}}</div> 
-                    <div class="col-xs-4">设置</div> 
-                </div>
-            </li>
-            <li class="safe-list-li">
-            <div class="container">
-                <div class="col-xs-4 text-left"><i class="fa fa-fw fa-exclamation-circle"></i>实名认证</div>
-                <div class="col-xs-4">{{userdata.certificationvalue}}</div> 
-                <div class="col-xs-4"><router-link to="/shimingrenzheng"> 认证 </router-link></div> 
-            </div>
             </li>
         </ul>
     </main>
@@ -52,75 +24,75 @@ let zhanghaoanquan = Vue.component('account-safe',{
     data:function(){
         return {
             userdatatemp:[{
-                emaillt:'绑定邮箱',emailvalue:' ',emailrt:' ',classname:' '
+                lt:'绑定邮箱',value:' ',rt:' ',classname:'xxx'
             },{
-                phonelt:' ',phonevalue:' ',phonert:' ',classname:' '
+                lt:' ',value:' ',rt:' ',classname:' '
             },{
-                passwordlt:' ',passwordvalue:' ',passwordrt:' ',classname:' '
+                lt:' ',value:' ',rt:' ',classname:' '
             },{ 
-                questionlt:' ',questionvalue:' ',questionrt:' ',classname:' '
+                lt:' ',value:' ',rt:' ',classname:' '
             },{
-                certificationlt:' ',certificationvalue:' ',certificationrt:' ',classname:''
+                lt:' ',value:' ',rt:' ',classname:''
             }]
             }
         },
-    mounted:function(){
-        console.log(this.userdatatemp)
-        this.userdatatemp[0].emaillt="绑定邮箱"
-        this.userdatatemp[1].phonelt="绑定手机"
-        this.userdatatemp[2].passwordlt="设置密码"
-        this.userdatatemp[3].questionlt="密保问题"
-        this.userdatatemp[4].certificationlt="实名认证"
+    created:function(){
+        console.log(this.userdata)
+        this.userdatatemp[0].lt="绑定邮箱"
+        this.userdatatemp[1].lt="绑定手机"
+        this.userdatatemp[2].lt="设置密码"
+        this.userdatatemp[3].lt="密保问题"
+        this.userdatatemp[4].lt="实名认证"
 
         if(this.userdata.emailif != 1){
-            this.userdatatemp[0].emailvalue="未绑定邮箱"
-            this.userdatatemp[0].emailrt="绑定邮箱"
+            this.userdatatemp[0].value="未绑定邮箱"
+            this.userdatatemp[0].rt="绑定邮箱"
             this.userdatatemp[0].classname="fa fa-fw fa-exclamation-circle"
         }else{
-            this.userdatatemp[0].emailvalue=this.userdata.emailvalue
-            this.userdatatemp[0].emailrt="更换邮箱"
+            this.userdatatemp[0].value=this.userdata.emailvalue
+            this.userdatatemp[0].rt="更换邮箱"
             this.userdatatemp[0].classname="fa fa-fw fa-check-circle"
         }
 
         if(this.userdata.phoneif != 1){
-            this.userdatatemp[1].phonevalue="未绑定手机"
-            this.userdatatemp[1].phonert="绑定手机"
+            this.userdatatemp[1].value="未绑定手机"
+            this.userdatatemp[1].rt="绑定手机"
             this.userdatatemp[1].classname="fa fa-fw fa-exclamation-circle"
         }else{
-            this.userdatatemp[1].phonevalue=this.userdata.emailvalue
-            this.userdatatemp[1].phonert="更换邮箱"
+            this.userdatatemp[1].value=this.userdata.phonevalue
+            this.userdatatemp[1].rt="更换邮箱"
             this.userdatatemp[1].classname="fa fa-fw fa-check-circle"
         }
 
         if(this.userdata.passwordif != 1){
-            this.userdatatemp[2].passwordvalue="未设置密码"
-            this.userdatatemp[2].passwordrt="设置密码"
+            this.userdatatemp[2].value="未设置密码"
+            this.userdatatemp[2].rt="设置密码"
             this.userdatatemp[2].classname="fa fa-fw fa-exclamation-circle"
         }else{
-            this.userdatatemp[2].passwordvalue="已设置密码"
-            this.userdatatemp[2].passwordrt="更换密码"
+            this.userdatatemp[2].value="已设置密码"
+            this.userdatatemp[2].rt="更换密码"
             this.userdatatemp[2].classname="fa fa-fw fa-check-circle"
         }
 
-        if(this.userdata.questionif != 1){
-            this.userdatatemp[3].questionvalue="未设置密保问题"
-            this.userdatatemp[3].questionrt="设置"
+        if(this.userdata.questuinif != 1){
+            this.userdatatemp[3].value="未设置密保问题"
+            this.userdatatemp[3].rt="设置"
             this.userdatatemp[3].classname="fa fa-fw fa-exclamation-circle"
         }else{
-            this.userdatatemp[3].questionvalue="已设置密保问题"
-            this.userdatatemp[3].questionrt="更换"
+            this.userdatatemp[3].value="已设置密保问题"
+            this.userdatatemp[3].rt="更换"
             this.userdatatemp[3].classname="fa fa-fw fa-check-circle"
         }
 
         if(this.userdata.certificationif != 1){
-            this.userdatatemp[4].certificationvalue="未实名认证"
-            this.userdatatemp[4].certificationrt="查看认证"
+            this.userdatatemp[4].value="未实名认证"
+            this.userdatatemp[4].rt="查看认证"
             this.userdatatemp[4].classname="fa fa-fw fa-check-circle"
         }else{
-            this.userdatatemp[4].certificationvalue="已实名认证"
-            this.userdatatemp[4].certificationrt="未认证"
+            this.userdatatemp[4].value="已实名认证"
+            this.userdatatemp[4].rt="未认证"
             this.userdatatemp[4].classname="fa fa-fw fa-check-circle"
         }
-        console.log(userdatatemp)
+        console.log(this.userdatatemp)
     }
 })
